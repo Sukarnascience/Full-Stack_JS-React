d23bbe5914150b45eec60af47d1f738dbcdef486
Sukarna Jana <sukarnascience@gmail.com>
2021-06-19 21:59:52 +0530

    useEffect started

--- demo/src/hookComponents/MotherHookToApp.js
+++ demo/src/hookComponents/MotherHookToApp.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom'
 import HooksStyle from './MyHooksPageStyle.module.css'
 
 import {CounterOne,CounterTwo,CounterObject,CounterArray} from './useStatepartone'
+//import {} from './useEffectHooksJS'
 
 function MotherHookToApp(){
     return ReactDOM.createPortal(
@@ -15,11 +16,12 @@ function MyHooks(){
     return(
         <div class={HooksStyle.HooksStylePG}>
             <h1>All your Hooks Component Now</h1>
-            <p>UseState Hook</p>
+            <p>useState Hook</p>
             <CounterOne/>
             <CounterTwo/>
             <CounterObject/>
             <CounterArray/>
+            <p>useEffect Hook</p>
         </div>
     )
 }
